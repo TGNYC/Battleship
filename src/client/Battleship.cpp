d7b7bab8e6fa4bcc5331ca6d04588d08160e3432
ariess <ariess@student.ethz.ch>
2023-04-19 11:04:08 +0200

    make GameWindow pointer unique

--- src/client/Battleship.cpp
+++ src/client/Battleship.cpp
@@ -1,9 +1,11 @@
 #include "Battleship.h"
 
 #include "GameWindow.h"
+#include <memory>
 
 auto Battleship::OnInit() -> bool {
-  auto *gameWindow = new GameWindow("Battleship", wxDefaultPosition, wxDefaultSize);
+  
+  std::unique_ptr<GameWindow> gameWindow = std::make_unique<GameWindow>("Battleship", wxDefaultPosition, wxDefaultSize);
   gameWindow->Show(true);
 
   return true;
