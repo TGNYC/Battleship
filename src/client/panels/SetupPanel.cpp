1a3c433b4c566651437e7f5f2ab9344400a5ece0
ariess <ariess@student.ethz.ch>
2023-04-27 23:32:15 +0200

    create SetupPanel class

--- src/client/panels/SetupPanel.cpp
+++ src/client/panels/SetupPanel.cpp
@@ -1,1 +1,14 @@
 #include "SetupPanel.h"
+
+SetupPanel::SetupPanel(wxWindow *parent) : wxPanel(parent) {
+  wxColor backgroundColor = wxColor(255, 255, 255);
+  this->setBackgroundColor(backgroundColor);
+
+  wxBoxSizer *verticalLayout = new wxBoxSizer(wxVERTICAL);
+
+  // TODO: add ship placement grid
+
+  // TODO: add ship selection 
+
+  this->_readyButton = new wxButton(this, wxID_ANY, "Ready", wxDefaultPosition, wxSize(100, 40));
+}
--- src/client/panels/SetupPanel.h
+++ src/client/panels/SetupPanel.h
@@ -1,6 +1,14 @@
 #ifndef SETUPPANEL_H
 #define SETUPPANEL_H
 
+#include <wx/wx.h>
 
+class SetupPanel : public wxPanel {
+public:
+  SetupPanel(wxWindow *parent);
+
+private:
+  wxButton *_readyButton;
+};
 
 #endif // SETUPPANEL_H
