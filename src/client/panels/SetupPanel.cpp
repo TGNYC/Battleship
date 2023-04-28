36cff0c7c52792dc2ae35d47c24417600ba0195a
ariess <ariess@student.ethz.ch>
2023-04-28 14:43:12 +0200

    make layout horizontal

--- src/client/panels/SetupPanel.cpp
+++ src/client/panels/SetupPanel.cpp
@@ -4,17 +4,17 @@ SetupPanel::SetupPanel(wxWindow *parent) : wxPanel(parent) {
   wxColor backgroundColor = wxColor(255, 255, 255);
   this->SetBackgroundColour(backgroundColor);
 
-  wxBoxSizer *verticalLayout = new wxBoxSizer(wxVERTICAL);
+  wxBoxSizer *horizontalLayout = new wxBoxSizer(wxHORIZONTAL);
 
   this->_placementGrid = new PlacementGrid(this);
-  verticalLayout->Add(this->_placementGrid, 1, wxDEFAULT, 10);
+  horizontalLayout->Add(this->_placementGrid, 1, wxDEFAULT, 10);
 
   // TODO: add ship selection
 
   this->_readyButton = new wxButton(this, wxID_ANY, "Ready", wxDefaultPosition, wxSize(100, 40));
 
-  verticalLayout->Add(this->_readyButton, 0, wxALIGN_CENTER, 10);
-  this->SetSizerAndFit(verticalLayout);
-  //verticalLayout->Layout();
+  horizontalLayout->Add(this->_readyButton, 0, wxALIGN_BOTTOM, 10);
+  this->SetSizerAndFit(horizontalLayout);
+  //horizontalLayout->Layout();
 
 }
