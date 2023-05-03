d5cfa638231b44122223756646fee62a0f9a111e
ariess <ariess@student.ethz.ch>
2023-05-03 13:19:46 +0200

    create demo MainGamePanel

--- src/client/panels/MainGamePanel.h
+++ src/client/panels/MainGamePanel.h
@@ -2,10 +2,20 @@
 #define MAINGAMEPANEL_H
 
 #include <wx/wx.h>
+#include "../../common/game_state/game_state.h"
+#include "../../common/game_state/Player.h"
+#include "../uiElements/ViewGrid.h"
 
 class MainGamePanel : public wxPanel {
 public:
   MainGamePanel(wxWindow *parent);
+  void buildGameState(game_state* gameState, Player* me);
+  void buildEmoteList();
+  void buildTurnIndicator();
+
+private:
+  ViewGrid* _ownViewGrid;
+  ViewGrid* _opponentViewGrid;
 };
 
 #endif // MAINGAMEPANEL_H
