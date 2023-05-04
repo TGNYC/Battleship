9ff7c26f3b325759ffd9d5f6147d5a11f7944291
Nico Müller <nicolmueller@ethz.ch>
2023-05-04 15:13:42 +0200

    added simple logger class

---
+++ src/common/Logger.cpp
@@ -0,0 +1,26 @@
+//
+// Created by nico on 04.05.23.
+//
+
+#include "Logger.h"
+
+const char* Logger::logFile = "log.txt";
+
+std::string Logger::getCurrentDateTime() {
+  auto t = std::time(nullptr);
+  auto tm = *std::localtime(&t);
+  std::ostringstream oss;
+  oss << std::put_time(&tm, "%d-%m-%Y %H-%M-%S");
+  return oss.str();
+}
+
+void Logger::log(const char *message, Logger::Type type) {
+  std::ofstream file;
+  try {
+    file.open(logFile);
+    file << getCurrentDateTime() << std::setw(15) << message << std::endl;
+    file.close();
+  } catch (const std::ofstream::failure& e) {
+    std::cout << "Error: Could not open/write to log file" << std::endl;
+  }
+}
---
+++ src/common/Logger.h
@@ -0,0 +1,44 @@
+//
+// Created by nico on 04.05.23.
+//
+
+#ifndef BATTLESHIP_LOGGER_H
+#define BATTLESHIP_LOGGER_H
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <ctime>
+#include "sstream"
+
+/*!
+ * Basic logging class. Only static usage
+ */
+class Logger {
+
+public:
+
+  enum Type {
+    info,
+    warning,
+    error
+  };
+
+  /*!
+   * outputs a message to the logfile
+   * @param message text to be logged
+   * @param type optional log type (info, warning, error). currently unused
+   */
+  static void log(const char* message, Type type = Type::info);
+
+private:
+  /*!
+   * Helper function for logging
+   * @return returns a formatted string timestamp
+   */
+  static std::string getCurrentDateTime();
+  static const char* logFile;
+};
+
+#endif // BATTLESHIP_LOGGER_H
