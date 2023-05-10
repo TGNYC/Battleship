cbabb17d6fc782d3b6e600828a3c58bd92ec4898
Tejas Gupta <TGNYC@protonmail.com>
2023-05-10 17:47:15 +0200

    Add server responses and wrote request_handler() for most functions (excl. emotes and quitgame)

---
+++ src/common/network/responses/RequestResponse.h
@@ -0,0 +1,16 @@
+//
+// Created by Tejas Gupta on 5/10/23.
+//
+
+#ifndef BATTLESHIP_REQUESTRESPONSE_H
+#define BATTLESHIP_REQUESTRESPONSE_H
+
+
+
+class RequestResponse {
+
+};
+
+
+
+#endif //BATTLESHIP_REQUESTRESPONSE_H
