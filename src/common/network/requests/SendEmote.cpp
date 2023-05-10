0010702b0df41ec226f3dc078e7759b567949bbd
Nico Müller <nicolmueller@ethz.ch>
2023-05-10 15:36:07 +0200

    bug fix: added back constructor

--- src/common/network/requests/SendEmote.cpp
+++ src/common/network/requests/SendEmote.cpp
@@ -8,6 +8,10 @@
 SendEmote::SendEmote(uuid playerId, EmoteHandler::Emote emote)
     : ClientRequest(playerId, RequestType::SendEmote), m_emote(emote) {}
 
+//TODO remove
+SendEmote::SendEmote(uuid playerId, std::string emote)
+: ClientRequest(playerId, RequestType::SendEmote), m_emote(EmoteHandler::MiddleFinger) {}
+
 auto SendEmote::getEmote() const -> EmoteHandler::Emote {
   return m_emote;
 }
