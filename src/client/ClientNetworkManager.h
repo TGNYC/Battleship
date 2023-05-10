74ae90ddc09564a7bf8c5cb4d1a2f76f9c0170d6
Mike Schmid <mike@schmid-plus.ch>
2023-05-10 11:50:15 +0200

    add ClientNetworkManager and TesponseListenerThread

--- src/client/ClientNetworkManager.h
+++ src/client/ClientNetworkManager.h
@@ -1,4 +1,28 @@
 #ifndef CLIENTNETWORKMANAGER_H
 #define CLIENTNETWORKMANAGER_H
 
+#include "network/requests/ClientRequest.h"
+#include "network/responses/ServerResponse.h"
+#include "sockpp/tcp_connector.h"
+#include <memory>
+#include <string>
+
+class ClientNetworkManager {
+
+public:
+  static void init(const std::string &host, const uint16_t port);
+
+  static void sendRequest(const ClientRequest &request);
+
+  static std::unique_ptr<ServerResponse> parseResponse(const std::string &message);
+
+private:
+  static bool connect(const std::string &host, const uint16_t port);
+
+  static sockpp::tcp_connector *_connection;
+
+  static bool _connectionSuccess;
+  static bool _failedToConnect;
+};
+
 #endif // CLIENTNETWORKMANAGER_H
---
+++ src/client/ResponseListenerThread.h
@@ -0,0 +1,22 @@
+#ifndef BATTLESHIP_RESPONSELISTENERTHREAD_H
+#define BATTLESHIP_RESPONSELISTENERTHREAD_H
+
+#include <sockpp/tcp_connector.h>
+#include <wx/wx.h>
+
+class ResponseListenerThread : public wxThread {
+
+public:
+  ResponseListenerThread(sockpp::tcp_connector *connection);
+  ~ResponseListenerThread();
+
+protected:
+  virtual ExitCode Entry();
+
+private:
+  void outputError(std::string title, std::string message);
+
+  sockpp::tcp_connector *_connection;
+};
+
+#endif // BATTLESHIP_RESPONSELISTENERTHREAD_H
