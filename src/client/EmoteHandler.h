4ceaed83fb355431df01898fda564e48b2258211
Nico Müller <nicolmueller@ethz.ch>
2023-05-11 14:40:22 +0200

    Changed location of emote enum to be in common package. Adapted classes using this enum

--- src/client/EmoteHandler.h
+++ src/client/EmoteHandler.h
@@ -3,21 +3,16 @@
 
 #include <string>
 #include <map>
+#include "network/responses/EmoteEvent.h"
 
 class EmoteHandler {
 public:
-  enum Emote {
-    MiddleFinger,
-    RussianWarshipGoFuckYourself,
-    Mocking,
-  };
-
-  static std::string getImage(Emote emote);
-  static std::string getSound(Emote emote);
+  static std::string getImage(EmoteType emote);
+  static std::string getSound(EmoteType emote);
 
 private:
-  static const std::map<Emote, std::string> images; ///< Mapping emotes to their visuals. For audio-only a clear png will be mapped
-  static const std::map<Emote, std::string> sounds; ///< Mapping emotes to their sound effects. For visual-only an empty WAV will be mapped
+  static const std::map<EmoteType, std::string> images; ///< Mapping emotes to their visuals. For audio-only a clear png will be mapped
+  static const std::map<EmoteType, std::string> sounds; ///< Mapping emotes to their sound effects. For visual-only an empty WAV will be mapped
 };
 
 #endif // EMOTEHANDLER_H
