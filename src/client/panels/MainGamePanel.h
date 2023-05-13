b20402d764a6de456832d24207294106af74d42a
ariess <ariess@student.ethz.ch>
2023-05-13 12:11:06 +0200

    Add comments for documentation

--- src/client/panels/MainGamePanel.h
+++ src/client/panels/MainGamePanel.h
@@ -11,11 +11,36 @@
 
 class MainGamePanel : public wxPanel {
 public:
+  /**
+   * @brief Constructor for the main game panel.
+   * Doesn't display anything, only the background color.
+  */
   MainGamePanel(wxWindow *parent);
+  /**
+   * @brief Builds the game state.
+   * @param gameState The game state.
+   * @param ownId The id of the player who is using this client.
+   * Displays the game state using ViewGrid and ShipPanel.
+  */
   void buildGameState(game_state* gameState, uuid ownId);
+  /**
+   * @brief Displays the emote panel.
+   * Not implemented yet. 
+  */
   void buildEmoteList();
+  /**
+   * @brief Displays the turn indicator: It's ((playerName))'s turn
+   * @param playerName The name of the player who's turn it is.
+   * @param header The wxBoxSizer that contains the turn indicator.
+  */
   void buildTurnIndicator(std::string playerName, wxBoxSizer *header);
-
+  /**
+   * @brief Function that is bound to the tiles of the opponent's view grid.
+   * @param event The mouse event.
+   * Makes sure that it's the player's turn and that the player can't shoot more than once per second.
+   * Uses the position of the mouse pointer to find out which tile was clicked.
+   * TODO: Calls the function shotIsLegal and sends callShot request to server.
+  */
   void onMouseClick(wxMouseEvent& event);
 
 private:
