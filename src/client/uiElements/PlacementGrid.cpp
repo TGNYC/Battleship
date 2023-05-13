b3c454272789d1de519c124d19301ad44a589a09
Robin <rjs02@hotmail.com>
2023-05-13 20:08:18 +0200

    code clean-up + add comments

--- src/client/uiElements/PlacementGrid.cpp
+++ src/client/uiElements/PlacementGrid.cpp
@@ -7,15 +7,16 @@
 #include "../SetupManager.h"
 
 
+/**
+ * @brief constructor for PlacementGrid. Creates grid of 10x10 tiles and binds mouse events
+ * @param parent
+ */
 PlacementGrid::PlacementGrid(wxWindow *parent) : wxPanel(parent, wxID_ANY, wxDefaultPosition, wxSize(400, 400)) {
-  wxColor backgroundColor = wxColor(255, 255, 0);
+  wxColor backgroundColor = wxColor(255, 255, 0); // yellow, for debugging, shouldn't be visible
   this->SetBackgroundColour(backgroundColor);
 
   constexpr int gridSize = 10;
-
-  //wxStaticBitmap* grid[10][10];
   this->_grid = new wxStaticBitmap*[gridSize*gridSize];
-
 
   wxBitmap emptyTile = wxBitmap(wxImage("../assets/empty_tile.png"));
   for (int i = 0; i < gridSize; i++) {
@@ -31,16 +32,22 @@ PlacementGrid::PlacementGrid(wxWindow *parent) : wxPanel(parent, wxID_ANY, wxDef
   Bind(wxEVT_LEFT_DOWN, &PlacementGrid::OnMouseClick, this);
 }
 
+/**
+ * @brief function is called when mouse hovers over grid and highlights tiles according to mouse position and placed ships
+ * @param event mouse event
+ */
 void PlacementGrid::OnMouseMotion(wxMouseEvent &event) {
   wxPoint mousePosition = event.GetPosition();
   int CellX = mousePosition.x / 40;
   int CellY = mousePosition.y / 40;
-  //std::cout << "(" << CellX << ", " << CellY << ")\n";
 
   if (CellX == cellX_prev && CellY == cellY_prev) { // mouse still in same cell as before
     return;
   }
   if (SetupManager::_selectedShip == nullptr) { // no ship selected
+    return;
+  }
+  if(SetupManager::_selectedShip->getPosition().x != -1 && SetupManager::_selectedShip->getPosition().y != -1) { // already placed
     return;
   }
   if (SetupManager::_selectedShip->getOrientation() == Ship::Orientation::Horizontal &&
@@ -52,39 +59,16 @@ void PlacementGrid::OnMouseMotion(wxMouseEvent &event) {
     // return;
     CellY = std::min(CellY, 10 - SetupManager::_selectedShip->getLength());
   }
-  if(SetupManager::_selectedShip->getPosition().x != -1 && SetupManager::_selectedShip->getPosition().y != -1) { // already placed
-    return;
-  }
 
-  // grid is column major!
   displayGrid();
 
-  int l = SetupManager::_selectedShip->getLength();
+  const int l = SetupManager::_selectedShip->getLength();
   auto orientation = SetupManager::_selectedShip->getOrientation();
-  //orientation = Ship::Orientation::Vertical;
-  // clear previous highlighted tiles
-  /*
-  if(cellX_prev != -1 && cellY_prev != -1) {
-    if(orientation == Ship::Orientation::Horizontal) {
-      for(int i = 0; i < l; i++) {
-        int idx = (cellX_prev+i)*10+cellY_prev;
-        if(cellX_prev + i < 10 && cellY_prev < 10)
-          _grid[idx]->SetBitmap(wxBitmap(wxImage("../assets/empty_tile.png")));
-      }
-    }
-    else if(orientation == Ship::Orientation::Vertical) {
-      for(int i = 0; i < l; i++) {
-        int idx = cellX_prev*10+cellY_prev+i;
-        if(cellX_prev < 10 && cellY_prev + i < 10)
-          _grid[idx]->SetBitmap(wxBitmap(wxImage("../assets/empty_tile.png")));
-      }
-    }
-  }
-*/
+
   // highlight new tiles
   if (orientation == Ship::Orientation::Horizontal) {
     for (int i = 0; i < l; i++) {
-      int idx = (CellX + i) * 10 + CellY;
+      int idx = (CellX + i) * 10 + CellY; // column-major order
       if (CellX + i < 10 && CellY < 10)
         _grid[idx]->SetBitmap(wxBitmap(wxImage("../assets/ship_tile.png")));
     }
@@ -98,12 +82,12 @@ void PlacementGrid::OnMouseMotion(wxMouseEvent &event) {
 
   cellX_prev = CellX;
   cellY_prev = CellY;
-
-  //this->displayGrid();
-
-  //this->_grid[CellX*10+CellY]->SetBitmap(wxBitmap(wxImage("../assets/ship_tile.png")));
-
 }
+
+/**
+ * @brief function is called when mouse clicks on grid and places ship if possible
+ * @param event
+ */
 void PlacementGrid::OnMouseClick(wxMouseEvent &event) {
   if(SetupManager::_selectedShip == nullptr) {
     std::cout << "no ship selected" << std::endl;
@@ -120,9 +104,11 @@ void PlacementGrid::OnMouseClick(wxMouseEvent &event) {
   std::cout << "placed ship\n";
 
   this->displayGrid();
-
-
 }
+
+/**
+ * @brief function displays grid according to data in SetupManager
+ */
 void PlacementGrid::displayGrid() {
   auto *grid = SetupManager::getGrid();
   for(int i = 0; i < 100; ++i) {
