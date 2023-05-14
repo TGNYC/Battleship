d1dcbba66e8291b7563e317168fb6b798f2391a6
ariess <ariess@student.ethz.ch>
2023-05-14 18:07:58 +0200

    change size of displayed emote

--- src/client/uiElements/EmotePanel.cpp
+++ src/client/uiElements/EmotePanel.cpp
@@ -1,5 +1,7 @@
 #include "EmotePanel.h"
 
+#include <iostream>
+
 EmotePanel::EmotePanel(wxWindow *parent, wxPoint pos) : wxPanel(parent, wxID_ANY, wxDefaultPosition, wxSize(100, 600)) {
     _parent = parent;
     // wxColor backgroundColor = wxColor(255, 0, 255);
@@ -16,8 +18,10 @@ EmotePanel::EmotePanel(wxWindow *parent, wxPoint pos) : wxPanel(parent, wxID_ANY
 }
 
 void EmotePanel::displayEmote(EmoteType emote) {
-    _currentEmote = new wxStaticBitmap(_parent, wxID_ANY, wxBitmap(wxImage(EmoteHandler::getImage(emote))), wxPoint(200, 200), wxSize(800, 800));
+    _currentEmote = new wxStaticBitmap(_parent, wxID_ANY, wxBitmap(wxImage(EmoteHandler::getImage(emote))), wxPoint(20, 300), wxSize(80, 80));
     _currentEmote->Bind(wxEVT_LEFT_DOWN, [this](wxMouseEvent& event) {
+        std::cout << "Klicked on emote" << std::endl;
         // TODO: find a way to remove the image
+        // _currentEmote = nullptr;
     });
 }
