4e0922f02c7562f8dda1018e497753aeda2abf34
Nico Müller <nicolmueller@ethz.ch>
2023-05-15 15:44:53 +0200

    formatting

--- src/server/server_network_manager.h
+++ src/server/server_network_manager.h
@@ -18,22 +18,20 @@
 
 class server_network_manager {
 private:
-  inline static std::shared_mutex    _rw_lock;
+  inline static std::shared_mutex _rw_lock;
   inline static sockpp::tcp_acceptor _acc;
 
   inline static std::unordered_map<std::string, sockpp::tcp_socket> _address_to_socket;
 
-  void                                                connect(uint16_t port);
-  static ssize_t                                      send_message(const std::string &msg, const std::string &address);
+  void connect(uint16_t port);
+  static ssize_t send_message(const std::string &msg, const std::string &address);
   inline static std::unordered_map<uuid, std::string> _player_id_to_address;
 
-  static void
-       read_message(sockpp::tcp_socket                                                                  socket,
+  static void read_message(sockpp::tcp_socket socket,
                     const std::function<void(const std::string &, const sockpp::tcp_socket::addr_t &)> &message_handler);
   void handle_incoming_message(const std::string &msg, const sockpp::tcp_socket::addr_t &peer_address);
 
   game_instance _game_instance;
-
   uint16_t _port;
 
 public:
@@ -42,9 +40,9 @@ public:
 
   // Used to broadcast a server_response (e.g. a full_state_response) to all 'players' except 'exclude'
   static void broadcast_message(ServerResponse &msg, const std::vector<Player> &players, const Player *exclude);
-  static void send_message_to_other_server(const std::string &host, const uint16_t port, const std::string &message);
+  //static void send_message_to_other_server(const std::string &host, const uint16_t port, const std::string &message);
   static void on_player_left(uuid player_id);
-  void        listener_loop();
+  void listener_loop();
 };
 
 #endif // BATTLESHIP_SERVER_NETWORK_MANAGER_H
