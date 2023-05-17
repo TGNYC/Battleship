b90047f88d7105109a381ea1b1edb47566c5e096
Nico Müller <nicolmueller@ethz.ch>
2023-05-17 19:57:34 +0200

    change log formatting. change function name type from char* to string

--- src/common/Logger.cpp
+++ src/common/Logger.cpp
@@ -10,15 +10,15 @@ const char* const Logger::tab = "  "; // 2 spaces
 std::string Logger::prefix = "";
 
 std::string Logger::getCurrentDateTime() {
-  auto t = std::time(nullptr);
-  auto tm = *std::localtime(&t);
+  auto time = std::time(nullptr);
+  auto locTime = *std::localtime(&time);
   std::ostringstream oss;
-  oss << std::put_time(&tm, "%d-%m-%Y %H:%M:%S");
+  oss << std::put_time(&locTime, "%d-%m-%Y %H:%M:%S");
   return oss.str();
 }
 
-void Logger::log(const std::string& message, const char function[]) {
-  const std::string output = prefix + tab + function + tab + message; // build message
+void Logger::log(const std::string& message, const std::string &function) {
+  const std::string output = prefix + tab + "[" + function + "]" + tab + message; // build message
   std::cout << output << std::endl; // write to console
   std::ofstream out; // write to log file
   try {
