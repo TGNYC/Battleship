bcd3b6e82e2ac66a39b04f2347c08ce34c5baa25
Robin <rjs02@hotmail.com>
2023-05-19 21:21:39 +0200

    change to previous ship layout (absolute positioning)

--- src/client/uiElements/ShipPanel.cpp
+++ src/client/uiElements/ShipPanel.cpp
@@ -68,37 +68,21 @@ ShipPanel::ShipPanel(wxWindow *parent, wxPoint pos, const bool sunk[5]) : wxPane
   // set the minimum size of the panel to 400x100
   this->SetMinSize(wxSize(400, 100));
 
-  std::string filenames[5] = {
-      "../assets/ship_1.png",
-      "../assets/ship_2.png",
-      "../assets/ship_3.png",
-      "../assets/ship_4.png",
-      "../assets/ship_5.png"
-  };
-
-  std::string filenamesSunk[5] = {
-      "../assets/ship_1_sunk.png",
-      "../assets/ship_2_sunk.png",
-      "../assets/ship_3_sunk.png",
-      "../assets/ship_4_sunk.png",
-      "../assets/ship_5_sunk.png"
-  };
-
   wxColor backgroundColor = wxColor(255, 255, 255);
   this->SetBackgroundColour(backgroundColor);
 
-  wxGridSizer* gridSizer = new wxGridSizer(2, 3, 10, 10);
+  //wxGridSizer* gridSizer = new wxGridSizer(2, 3, 5, 5);
+  int positions[5][2] = {
+      {pos.x + 0*40, pos.y + 0*30},
+      {pos.x + 6*40, pos.y + 0*30},
+      {pos.x + 0*40, pos.y + 2*30},
+      {pos.x + 4*40, pos.y + 2*30},
+      {pos.x + 8*40, pos.y + 2*30}
+  };
+
+  int lengths[5] = {5, 4, 3, 3, 2};
 
   for (size_t i=0; i<5; ++i) {
-    wxStaticBitmap* bitmap;
-    if (sunk[i]) {
-      bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(wxImage(filenamesSunk[i])));
-    } else {
-      bitmap = new wxStaticBitmap(this, wxID_ANY, wxBitmap(wxImage(filenames[i])));
-    }
-    _shipBitmaps[i] = bitmap;
-    gridSizer->Add(bitmap, 1, wxEXPAND);
+    _shipBitmaps[i] = new wxStaticBitmap(this, wxID_ANY, wxBitmap(wxImage(_filenames[i])), wxPoint(positions[i][0], positions[i][1]), wxSize(lengths[i]*40, 40));
   }
-
-  this->SetSizer(gridSizer);
 }
--- src/client/uiElements/ShipPanel.h
+++ src/client/uiElements/ShipPanel.h
@@ -10,6 +10,21 @@ public:
     ShipPanel(wxWindow *parent, wxPoint pos, const bool sunk[5]);
 private:
     wxStaticBitmap* _shipBitmaps[5];
+    std::string _filenames[5] = {
+        "../assets/ship_1.png",
+        "../assets/ship_2.png",
+        "../assets/ship_3.png",
+        "../assets/ship_4.png",
+        "../assets/ship_5.png"
+    };
+
+    std::string _filenamesSunk[5] = {
+        "../assets/ship_1_sunk.png",
+        "../assets/ship_2_sunk.png",
+        "../assets/ship_3_sunk.png",
+        "../assets/ship_4_sunk.png",
+        "../assets/ship_5_sunk.png"
+    };
 };
 
 #endif // SHIPPANEL_H
