cf1c0829cc74610640386e970efa5d3b06913758
ariess <ariess@student.ethz.ch>
2023-05-24 15:02:21 +0200

    panic!

--- src/client/EmoteHandler.cpp
+++ src/client/EmoteHandler.cpp
@@ -4,14 +4,16 @@ const std::map<EmoteType, std::string> EmoteHandler::_images = {
     {EmoteType::MiddleFinger, "../assets/emotes/middlefinger.png"},
     {EmoteType::RussianWarshipGoFuckYourself, "../assets/emotes/gofuckyourself.png"},
     {EmoteType::Mocking, "../assets/emotes/mocking.png"},
-    {EmoteType::BestPirate, "../assets/emotes/bestpirate.png"}
+    {EmoteType::BestPirate, "../assets/emotes/bestpirate.png"},
+    {EmoteType::Panic, "../assets/emotes/panic.png"}  
 };
 
 const std::map<EmoteType, std::string> EmoteHandler::_sounds = {
     {EmoteType::MiddleFinger, ""},
     {EmoteType::RussianWarshipGoFuckYourself, ""},
     {EmoteType::Mocking, ""},
-    {EmoteType::BestPirate, "../assets/audio/BestPirate.wav"}
+    {EmoteType::BestPirate, "../assets/audio/BestPirate.wav"},
+    {EmoteType::Panic, ""}
 };
 
 std::string EmoteHandler::getImage(EmoteType emote) {
--- src/common/network/responses/EmoteEvent.h
+++ src/common/network/responses/EmoteEvent.h
@@ -9,7 +9,8 @@ enum class EmoteType {
   MiddleFinger, // value 0
   RussianWarshipGoFuckYourself, // 1
   Mocking, // 2
-  BestPirate // 3
+  BestPirate, // 3
+  Panic // 4
 };
 
 class EmoteEvent : public ServerResponse {
